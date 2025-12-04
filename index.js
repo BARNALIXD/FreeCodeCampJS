1a66340c635c17abfcf02aa548b4c148969297d6
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-12-04 20:32:13 +0530

    similar

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 //Example
 var lastName = "samikshya";
+var LastChar = lastName[lastName.length - 1];;
+console.log(LastChar);
+
+//Task
+//Use bracket notation to find the last character in the lastName variable and assign it to the LastChar variable.
+var lastName = "Dasgupta";
 var LastChar = lastName[8];
 console.log(LastChar);
