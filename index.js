c5a5ee929145393ad1c699db8ea2801e2c22ea44
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-12-04 21:47:22 +0530

    Access Multi-Dimentional Arrays with the Indexes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var arr1 = [1, 2, 3];
-arr1[0] = 4;
-arr1[1] = 5;
-arr1[2] = 6;
-console.log(arr1); // Output: [4, 5, 6]
+var arr1 = [[1,2,3],[4,5,6],[7,8,9]];
+
+var element = arr1[1][2]; // Accessing the element '6' from the 2nd sub-array
+
+console.log(element); // Output: 6  
