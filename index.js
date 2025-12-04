2f0bd9df160d3afe66ff03a9d83147fa7c9792d3
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-12-04 20:49:32 +0530

    Nested Array

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var ourArray = ["John", 23];
-ourArray[0] = "Quincy";
-ourArray[1] = 45;
-console.log(ourArray);
+var arr1 = ["barnali", 23, true, [1, 2, 3]];
+console.log(arr1[3]);
 
+var arr2 = [["deep", "deeper"], ["deeper", "deepest"]];
