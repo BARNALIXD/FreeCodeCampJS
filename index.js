38c70b8e613d9a53d8c6be913901cd94a290ee17
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-12-04 20:50:18 +0530

    Access Array Data with Indexes

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var arr1 = ["barnali", 23, true, [1, 2, 3]];
-console.log(arr1[3]);
+
 
 var arr2 = [["deep", "deeper"], ["deeper", "deepest"]];
+console.log(arr2[0]); // Output: ["deep", "deeper"]
+console.log(arr2[1]); // Output: ["deeper", "deepest"]
+console.log(arr2[0][1]); // Output: "deeper"
+console.log(arr2[1][0]); // Output: "deeper"    
