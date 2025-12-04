d6d2724ab635d6279f6c7eaf6e015fd6776577d7
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-12-04 20:36:34 +0530

    Word Blanks

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var firstName = "Barnali";
-var ThirdLastLetter = firstName[firstName.length - 3];
-console.log(ThirdLastLetter);
+function wordBlanks(noun, verb, adjective, adverb) {
+    var result = "";
+    result += "The " + adjective + " " + noun + " " + verb + " to the store " + adverb + ".";
+    return result;
+}
+
+console.log(wordBlanks("dog", "ran", "quick", "quickly"));
+console.log(wordBlanks("cat", "jumped", "lazy", "slowly"));
