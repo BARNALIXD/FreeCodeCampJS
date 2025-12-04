eded41d46ea2beeb5a1b0b8b8d20875a3b902da7
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-12-04 20:29:54 +0530

    Bracket Notation to find the Nth character in String

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-var myStr = "barnali xd";
-myStr[0] = "B"; // this won't work
-console.log(myStr);
+//Example
+var firstName = "Charles";
+var secondLetterOfFirstName = firstName[1];
+
+var lastName = "Johnson";
+var thirdLetterOfLastName = lastName[2];
+
+console.log(secondLetterOfFirstName);
+console.log(thirdLetterOfLastName);
