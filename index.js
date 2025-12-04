13713df49c596931572bf8d4e08d248adc6ffb77
barnalixd <dasbarnali690@gcekbpatna.ac.in>
2025-12-04 20:34:19 +0530

    Bracket Notation to find the Nth-to-Last Character in the String

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-//Example
-var lastName = "samikshya";
-var LastChar = lastName[lastName.length - 1];;
-console.log(LastChar);
-
-//Task
-//Use bracket notation to find the last character in the lastName variable and assign it to the LastChar variable.
-var lastName = "Dasgupta";
-var LastChar = lastName[8];
-console.log(LastChar);
+var firstName = "Barnali";
+var ThirdLastLetter = firstName[firstName.length - 3];
+console.log(ThirdLastLetter);
