b3ac6d92f831a14badbf77f6dd05f37447ad4a8c
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-12-06 15:55:29 +0530

    Shopping List

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var a = [1, 2, 3];
-var remove = a.unshift(4, 5);
-console.log(a);
-console.log(remove);
-
-// The unshift() method adds new items to the beginning of an array and returns the new length.
-// In the example above, we added 4 and 5 to the beginning of the array a. The new length is 5.
-// Note: The unshift() method changes the original array.
+var myList = [
+  ["Chocolate Bar", 15],
+  ["Ice Cream", 10],
+  ["Bananas", 5],
+  ["Eggs", 12],
+  ["Bread", 2]
+];
+console.log(myList);
