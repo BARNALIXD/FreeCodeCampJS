d69547e81d19e88c17c89c7c43a4c262fb1d8312
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-12-06 20:24:34 +0530

    functions with arguements

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ function multiplication(a, b) {
     return a * b;
 }
 
-
 console.log(multiplication(4, 5));
 
+function division(a, b) {
+    if (b === 0) {
+        return 'Error: Division by zero';
+    }
+    return a / b;
+}
+
+console.log(division(20, 4));
+console.log(division(10, 0));
+
+module.exports = { multiplication, division };  
