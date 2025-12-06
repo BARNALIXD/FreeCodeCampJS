23990514f192a9fe5dee69b0f23e6134aec07294
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-12-06 16:00:42 +0530

    Write Reusable codes with functions

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-var myList = [
-  ["Chocolate Bar", 15],
-  ["Ice Cream", 10],
-  ["Bananas", 5],
-  ["Eggs", 12],
-  ["Bread", 2]
-];
-console.log(myList);
+function read() {
+  console.log("Hello World");
+}
+
+read();
+read(); 
+read();
+
+
+function reusablefunctionName() {
+    let a = 1;
+let b = 2;
+let sum = a + b;
+console.log(sum);
+}
+
+reusablefunctionName();
+reusablefunctionName();
+reusablefunctionName(); 
+  //code to be executed
+  console.log("Hello World");
+  console.log();
+read();
+read();
+read();
+
+
+
+
+
+function addition() {
+    let a = 5;
+    let b = 10;
+    let sum = a + b;
+    console.log(sum);
+}
+
+addition();
+addition();
+addition();
