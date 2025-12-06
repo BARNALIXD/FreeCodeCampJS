c9bac0de2feb12931e7652807ed88ea4cf0e25ff
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2025-12-06 15:52:39 +0530

    Manipulate arrays with unshift( )

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var a = [1,2,3, "barnali", [4,5,6]];
-a.shift();
+var a = [1, 2, 3];
+var remove = a.unshift(4, 5);
 console.log(a);
+console.log(remove);
 
-//in pop we remove the last elemnt of the array and in shift we remove the first element of the array.
+// The unshift() method adds new items to the beginning of an array and returns the new length.
+// In the example above, we added 4 and 5 to the beginning of the array a. The new length is 5.
+// Note: The unshift() method changes the original array.
